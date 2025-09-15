808960d807935d83d3fb6fbf77a5620cf4159c40
Christian Schabesberger <chris.schabesberger@mailbox.org>
2025-09-15 21:03:57 +0200

    test views environment

--- test-app/src/androidTest/java/net/newpipe/newplayer/testapp/TestTest.kt
+++ test-app/src/androidTest/java/net/newpipe/newplayer/testapp/TestTest.kt
@@ -1,8 +1,11 @@
 package net.newpipe.newplayer.testapp
 
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import org.junit.Rule
@@ -16,14 +19,14 @@ class TestTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun testTest() {
-        composeTestRule.onNodeWithText("Start 6502 Stream").performClick()
-        /*
-        composeTestRule.waitUntil(timeoutMillis = 5000) {
-            assertEquals(5, 5)
-            true
-        }
-         */
+        //composeTestRule.onNodeWithText("Start 6502 Stream").performClick()
+        onView(ViewMatchers.withText("Start 6502 Stream")).perform(ViewActions.click())
+
     }
 }
